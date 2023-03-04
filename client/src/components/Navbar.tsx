842c40669040c65cf0729c30e99d856a0b89eef6
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-03-04 22:28:30 +0300

    fix: Wrong title on navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Navbar = () => {
   return (
     <nav className="flex justify-between bg-[#0A2647] p-4 py-6 text-white">
-      <h3 className="font-bold">Questup</h3>
+      <h3 className="font-bold">Quizaction</h3>
       <div>
         <ul className="flex gap-4">
           <li>
